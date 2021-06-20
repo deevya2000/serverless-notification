9476417473501ee5fe5c8bb81041cfbe15d7f2f9
divya.bendi@ibm.com <divya.bendi@ibm.com>
2021-06-19 23:52:21 -0400

    Test Country

--- src/main/java/com/ibm/ProductPriceResource.java
+++ src/main/java/com/ibm/ProductPriceResource.java
@@ -1,14 +1,20 @@
 package com.ibm;
 
+import java.util.Set;
+
 import javax.inject.Inject;
 import javax.ws.rs.GET;
 import javax.ws.rs.POST;
 import javax.ws.rs.Path;
-import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 
+import java.util.concurrent.CompletionStage;
+
 import org.eclipse.microprofile.rest.client.inject.RestClient;
+import org.jboss.resteasy.annotations.jaxrs.PathParam;
+
+import io.smallrye.mutiny.Uni;
 
 
 @Path("/")
@@ -53,5 +59,32 @@ public class ProductPriceResource {
 	public String hello() {
 		return "Hello! Notification service is accessible";
 	}
+	
+	///////////////
+	
+    @Inject
+    @RestClient
+    CountriesService countriesService;
+
+    @GET
+    @Path("/name/{name}")
+    @Produces(MediaType.APPLICATION_JSON)
+    public Set<Country> name(@PathParam String name) {
+        return countriesService.getByName(name);
+    }
+
+    @GET
+    @Path("/name-async/{name}")
+    @Produces(MediaType.APPLICATION_JSON)
+    public CompletionStage<Set<Country>> nameAsync(@PathParam String name) {
+        return countriesService.getByNameAsync(name);
+    }
+
+    @GET
+    @Path("/name-uni/{name}")
+    @Produces(MediaType.APPLICATION_JSON)
+    public Uni<Set<Country>> nameMutiny(@PathParam String name) {
+        return countriesService.getByNameAsUni(name);
+    }
 	
 }
