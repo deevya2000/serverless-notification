617f5b5eaeaec60c0f39b2cdeb9e9ef9b846aa34
divya.bendi@ibm.com <divya.bendi@ibm.com>
2021-06-19 23:01:34 -0400

    removed Rest client

--- src/main/java/com/ibm/EmailService.java
+++ src/main/java/com/ibm/EmailService.java
@@ -1,19 +1,20 @@
-package com.ibm;
-
-import javax.ws.rs.POST;
-import javax.ws.rs.Path;
-import javax.ws.rs.Produces;
-import javax.ws.rs.core.MediaType;
-
-import org.eclipse.microprofile.rest.client.inject.RegisterRestClient;
-
-@Path("/")
-@RegisterRestClient
-public interface EmailService {
-
-	@POST
-	@Path("/sendmail")
-	@Produces(MediaType.APPLICATION_JSON)
-	String sendemail(String payload);
-
-}
+/*
+ * package com.ibm;
+ * 
+ * import javax.ws.rs.POST; import javax.ws.rs.Path; import
+ * javax.ws.rs.Produces; import javax.ws.rs.core.MediaType;
+ * 
+ * import org.eclipse.microprofile.rest.client.inject.RegisterRestClient;
+ * 
+ * @Path("/")
+ * 
+ * @RegisterRestClient public interface EmailService {
+ * 
+ * @POST
+ * 
+ * @Path("/sendmail")
+ * 
+ * @Produces(MediaType.APPLICATION_JSON) String sendemail(String payload);
+ * 
+ * }
+ */
