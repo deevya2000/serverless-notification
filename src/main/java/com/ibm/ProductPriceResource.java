5a375121f96436c4c00b22fb7220d0de9ebb71e2
divya.bendi@ibm.com <divya.bendi@ibm.com>
2021-06-21 00:33:07 -0400

    Restclient change

--- src/main/java/com/ibm/ProductPriceResource.java
+++ src/main/java/com/ibm/ProductPriceResource.java
@@ -1,5 +1,12 @@
 package com.ibm;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
 import java.util.Set;
 
 import javax.inject.Inject;
@@ -19,11 +26,6 @@ import io.smallrye.mutiny.Uni;
 
 @Path("/")
 public class ProductPriceResource {
-	
-	
-	  @Inject	  
-	  @RestClient 
-	  EmailService emailService;
 	 
     
     @GET
@@ -57,8 +59,45 @@ public class ProductPriceResource {
 	@Path("/hello")
 	@Produces(MediaType.TEXT_PLAIN)
 	public String hello() {
-		String resp = emailService.sendemail("{\"name\":\"test\",\"salary\":\"123\",\"age\":\"23\"}");
-	    System.out.println("Serverless Response = "+resp);	
+	      try {
+
+	          URL url = new URL("http://dummy.restapiexample.com/api/v1/create");
+	          HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+	          conn.setDoOutput(true);
+	          conn.setRequestMethod("POST");
+	          conn.setRequestProperty("Content-Type", "application/json");
+
+	          String input = "{\"name\":\"test\",\"salary\":\"123\",\"age\":\"23\"}";
+
+	          OutputStream os = conn.getOutputStream();
+	          os.write(input.getBytes());
+	          os.flush();
+
+	          if (conn.getResponseCode() != HttpURLConnection.HTTP_OK) {
+	              throw new RuntimeException("Failed : HTTP error code : "
+	                  + conn.getResponseCode());
+	          }
+
+	          BufferedReader br = new BufferedReader(new InputStreamReader(
+	                  (conn.getInputStream())));
+
+	          String output;
+	          System.out.println("Output from Server .... \n");
+	          while ((output = br.readLine()) != null) {
+	              System.out.println(output);
+	          }
+
+	          conn.disconnect();
+
+	        } catch (MalformedURLException e) {
+
+	          e.printStackTrace();
+
+	        } catch (IOException e) {
+
+	          e.printStackTrace();
+
+	       }	    
 		return "Hello! Notification service is accessible";
 	}
 	
