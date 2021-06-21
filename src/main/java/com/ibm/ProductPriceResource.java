b742968f1850a775f4e4a01b54808cd35cb7d0a7
divya.bendi@ibm.com <divya.bendi@ibm.com>
2021-06-20 23:27:32 -0400

    Test 1

--- src/main/java/com/ibm/ProductPriceResource.java
+++ src/main/java/com/ibm/ProductPriceResource.java
@@ -60,31 +60,4 @@ public class ProductPriceResource {
 		return "Hello! Notification service is accessible";
 	}
 	
-	///////////////
-	
-    @Inject
-    @RestClient
-    CountriesService countriesService;
-
-    @GET
-    @Path("/name/{name}")
-    @Produces(MediaType.APPLICATION_JSON)
-    public Set<Country> name(@PathParam String name) {
-        return countriesService.getByName(name);
-    }
-
-    @GET
-    @Path("/name-async/{name}")
-    @Produces(MediaType.APPLICATION_JSON)
-    public CompletionStage<Set<Country>> nameAsync(@PathParam String name) {
-        return countriesService.getByNameAsync(name);
-    }
-
-    @GET
-    @Path("/name-uni/{name}")
-    @Produces(MediaType.APPLICATION_JSON)
-    public Uni<Set<Country>> nameMutiny(@PathParam String name) {
-        return countriesService.getByNameAsUni(name);
-    }
-	
 }
